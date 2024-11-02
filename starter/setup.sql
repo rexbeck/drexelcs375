26c50f2995aba9f85b3cfba6cfca02582dc362c8
rexbeck <reaganrbeck@gmail.com>
2024-11-02 19:45:40 -0400

    Now generates users with "npm run setup"

--- starter/setup.sql
+++ starter/setup.sql
@@ -1,6 +1,8 @@
+DROP DATABASE IF EXISTS rsquared;
 CREATE DATABASE rsquared;
 \c rsquared
 
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
@@ -8,6 +10,7 @@ CREATE TABLE users (
     email VARCHAR(100) UNIQUE NOT NULL
 );
 
+DROP TABLE IF EXISTS posts;
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
     text TEXT,
@@ -16,6 +19,7 @@ CREATE TABLE posts (
     user_id INTEGER REFERENCES users(id)
 );
 
+DROP TABLE IF EXISTS comments;
 CREATE TABLE comments (
     id SERIAL PRIMARY KEY,
     text TEXT,
@@ -24,18 +28,21 @@ CREATE TABLE comments (
     post_id INTEGER REFERENCES posts(id)
 );
 
+DROP TABLE IF EXISTS likes;
 CREATE TABLE likes (
     user_id INTEGER REFERENCES users(id),
     post_id INTEGER REFERENCES posts(id),
     PRIMARY KEY (user_id, post_id)
 );
 
+DROP TABLE IF EXISTS friendships;
 CREATE TABLE friendships (
     user1_id INTEGER REFERENCES users(id),
     user2_id INTEGER REFERENCES users(id),
     PRIMARY KEY (user1_id, user2_id)
 );
 
+DROP TABLE IF EXISTS outfits;
 CREATE TABLE outfits (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),
@@ -43,6 +50,7 @@ CREATE TABLE outfits (
     user_id INTEGER REFERENCES users(id)
 );
 
+DROP TABLE IF EXISTS items;
 CREATE TABLE items (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),
@@ -50,8 +58,13 @@ CREATE TABLE items (
     image BOOLEAN
 );
 
+DROP TABLE IF EXISTS outfit_items;
 CREATE TABLE outfit_items (
     outfit_id INTEGER REFERENCES outfits(id),
     item_id INTEGER REFERENCES items(id),
     PRIMARY KEY (outfit_id, item_id)
 );
+
+INSERT INTO users (username, password, email) VALUES ('abc', 'mycoolpassword', 'abc@email.com');
+INSERT INTO users (username, password, email) VALUES ('admin', 'root', 'admin@email.com');
+INSERT INTO users (username, password, email) VALUES ('fiddlesticks', 'bibblebap', 'fiddlesticks@email.com');
