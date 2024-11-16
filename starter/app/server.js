4c28bf2810475be5d6c2b0eb07a1dd51ef76bc0d
rexbeck <reaganrbeck@gmail.com>
2024-11-16 18:02:26 -0500

    Finished feed, updated schemas, began adding routing

--- starter/app/server.js
+++ starter/app/server.js
@@ -1,6 +1,8 @@
 const pg = require("pg");
 const express = require("express");
 let cookieParser = require("cookie-parser");
+let argon2 = require("argon2");
+let crypto = require("crypto");
 const app = express();
 
 const port = 3000;
@@ -15,13 +17,14 @@ pool.connect().then(function () {
 
 app.use(express.static("public"));
 app.use(express.json());
-//app.use(cookieParser)
+app.use(cookieParser());
 
-// global object for storing tokens
-// in a real app, we'd save them to a db so even if the server exits
-// users will still be logged in when it restarts
-let tokenStorage = {};
-
+function isValidInput(name, category, image) {
+  if (!name || name.length > 15 || name.length < 1) return false;
+  if (!["shirt", "pants", "hat"].includes(category)) return false;
+  if (!["yes", "no"].includes(image)) return false;
+  return true;
+}
 
 app.post("/add", async (req, res) => {
   let { name, category, image } = req.body;
@@ -41,15 +44,6 @@ app.post("/add", async (req, res) => {
     res.status(500).send(); 
   }
 });
-
-
-function isValidInput(name, category, image) {
-  if (!name || name.length > 15 || name.length < 1) return false;
-  if (!["shirt", "pants", "hat"].includes(category)) return false;
-  if (!["yes", "no"].includes(image)) return false;
-  return true;
-}
-
 
 app.get("/search", async (req, res) => {
   let category = req.query.category?.toLowerCase(); 
@@ -75,12 +69,14 @@ app.get("/feed/:user", async (req, res) => {
   console.log("feed");
   let userId = req.params.user;
   console.log("userId:", userId);
-  pool.query(`SELECT * FROM friendships`).then(result => {
+  pool.query(`SELECT user2_id FROM friendships WHERE user1_id = ${userId}`).then(result => {
     console.log(`SELECT user2_id FROM friendships WHERE user1_id = ${userId}`);
     let friends = result.rows;
-    console.log(friends);
-    let friendIds = friends.map(id => friends.id);
-    let idListString = friendIds.map(() => '?').join(', ');
+    console.log("friends:",friends);
+    let friendIds = friends.map(friend => friend.user2_id);
+    console.log("friendIds:", friendIds);
+    let idListString = friendIds.map(id => `${id}`).join(', ');
+    console.log("idListString:", idListString);
 
     pool.query(`SELECT * FROM posts WHERE user_id IN (${idListString})`).then(result => {
       console.log(`SELECT * FROM posts WHERE user_id IN (${idListString})`);
@@ -89,11 +85,6 @@ app.get("/feed/:user", async (req, res) => {
   });
 });
 
-// // PASSWORD AUTHORIZATION & COOKIES
-
-// // must use same cookie options when setting/deleting a given cookie with res.cookie and res.clearCookie
-// // or else the cookie won't actually delete
-// // remember that the token is essentially a password that must be kept secret
 // let cookieOptions = {
 //   httpOnly: true, // client-side JS can't access this cookie; important to mitigate cross-site scripting attack damage
 //   secure: true, // cookie will only be sent over HTTPS connections (and localhost); important so that traffic sniffers can't see it even if our user tried to use an HTTP version of our site, if we supported that
@@ -111,56 +102,90 @@ app.get("/feed/:user", async (req, res) => {
 // });
 
 // app.get("/removecookie", (req, res) => {
-//   console.log(req.cookies);
-//   return res.clearCookie("abc", cookieOptions).send("Cookie removed");
+//     console.log(req.cookies);
+//     return res.clearCookie("abc", cookieOptions).send("Cookie removed");
 // });
 
-// /* returns a random 32 byte string */
+// // global object for storing tokens
+// // in a real app, we'd save them to a db so even if the server exits
+// // users will still be logged in when it restarts
+// let tokenStorage = {};
+
 // function makeToken() {
-//   return crypto.randomBytes(32).toString("hex");
+//     return crypto.randomBytes(32).toString("hex");
+//   }
+  
+// function validateLogin(body) {
+//     // TODO
+//     return true;
 // }
 
-// function validateLogin(body) {
-//   // TODO
-//   return true;
-// }
+// /* middleware; check if login token in token storage, if not, 403 response */
+// let authorize = (req, res, next) => {
+//     let { token } = req.cookies;
+//     console.log(token, tokenStorage);
+//     if (token === undefined || !tokenStorage.hasOwnProperty(token)) {
+//       return res.sendStatus(403); // TODO
+//     }
+//     next();
+// };
 
 // app.post("/create", async (req, res) => {
-//   let { body } = req;
+//     let { body } = req;
+  
+//     // TODO validate body is correct shape and type
+//     if (!validateLogin(body)) {
+//       return res.sendStatus(400); // TODO
+//     }
+  
+//     let { username, password } = body;
+//     console.log(username, password);
+  
+//     // TODO check username doesn't already exist
+//     // TODO validate username/password meet requirements
+  
+//     let hash;
+//     try {
+//       hash = await argon2.hash(password);
+//     } catch (error) {
+//       console.log("HASH FAILED", error);
+//       return res.sendStatus(500); // TODO
+//     }
+  
+//     console.log(hash); // TODO just for debugging
+//     try {
+//       await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+//         username,
+//         hash,
+//       ]);
+//     } catch (error) {
+//       console.log("INSERT FAILED", error);
+//       return res.sendStatus(500); // TODO
+//     }
+  
+//     // TODO automatically log people in when they create account, because why not?
+  
+//     return res.status(200).send(); // TODO
+// });
 
-//   // TODO validate body is correct shape and type
-//   if (!validateLogin(body)) {
-//     return res.sendStatus(400); // TODO
-//   }
-
-//   let { username, password } = body;
-//   console.log(username, password);
-
-//   // TODO check username doesn't already exist
-//   // TODO validate username/password meet requirements
-
-//   let hash;
-//   try {
-//     hash = await argon2.hash(password);
-//   } catch (error) {
-//     console.log("HASH FAILED", error);
-//     return res.sendStatus(500); // TODO
-//   }
-
-//   console.log(hash); // TODO just for debugging
-//   try {
-//     await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
-//       username,
-//       hash,
-//     ]);
-//   } catch (error) {
-//     console.log("INSERT FAILED", error);
-//     return res.sendStatus(500); // TODO
-//   }
-
-//   // TODO automatically log people in when they create account, because why not?
-
-//   return res.status(200).send(); // TODO
+// app.post("/logout", (req, res) => {
+//     let { token } = req.cookies;
+  
+//     if (token === undefined) {
+//       console.log("Already logged out");
+//       return res.sendStatus(400); // TODO
+//     }
+  
+//     if (!tokenStorage.hasOwnProperty(token)) {
+//       console.log("Token doesn't exist");
+//       return res.sendStatus(400); // TODO
+//     }
+  
+//     console.log("Before", tokenStorage);
+//     delete tokenStorage[token];
+//     console.log("Deleted", tokenStorage);
+  
+//     return res.clearCookie("token", cookieOptions).send();
 // });
 
 // app.post("/login", async (req, res) => {
@@ -211,46 +236,29 @@ app.get("/feed/:user", async (req, res) => {
 //   return res.cookie("token", token, cookieOptions).send(); // TODO
 // });
 
-// /* middleware; check if login token in token storage, if not, 403 response */
-// let authorize = (req, res, next) => {
-//   let { token } = req.cookies;
-//   console.log(token, tokenStorage);
-//   if (token === undefined || !tokenStorage.hasOwnProperty(token)) {
-//     return res.sendStatus(403); // TODO
-//   }
-//   next();
-// };
-
-// app.post("/logout", (req, res) => {
-//   let { token } = req.cookies;
-
-//   if (token === undefined) {
-//     console.log("Already logged out");
-//     return res.sendStatus(400); // TODO
-//   }
-
-//   if (!tokenStorage.hasOwnProperty(token)) {
-//     console.log("Token doesn't exist");
-//     return res.sendStatus(400); // TODO
-//   }
-
-//   console.log("Before", tokenStorage);
-//   delete tokenStorage[token];
-//   console.log("Deleted", tokenStorage);
-
-//   return res.clearCookie("token", cookieOptions).send();
-// });
-
 // app.get("/public", (req, res) => {
-//   return res.send("A public message\n");
+//     return res.send("A public message\n");
 // });
-
+  
 // // authorize middleware will be called before request handler
 // // authorize will only pass control to this request handler if the user passes authorization
 // app.get("/private", authorize, (req, res) => {
-//   return res.send("A private message\n");
+//     return res.send("A private message\n");
 // });
 
+/* BELOW IS FOR THE ROUTING MIDDLEWARE. KEPT GETTING AN ERROR SO HAD TO PUT IT ON PAUSE FOR NOW.
+const functionRoute = require('./routes/functionRoutes');
+const identityRoute = require('./routes/identityRoutes');
+app.use('/search', functionRoute);
+app.use('/add', functionRoute);
+app.use('/feed:userId', functionRoute);
+app.use('/create', identityRoute);
+app.use('/logout', identityRoute);
+app.use('/public', identityRoute);
+app.use('/private', identityRoute);
+app.use('/addcookie', identityRoute);
+app.use('/removecookie', identityRoute);
+*/
 
 app.listen(port, hostname, () => {
   console.log(`Listening at: http://${hostname}:${port}`);
