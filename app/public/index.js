5cfc1eec999b2025a2c372eee0bc22adaf9b5359
rexbeck <reaganrbeck@gmail.com>
2024-12-07 22:38:09 -0500

    Beginning of UI for feed

--- app/public/index.js
+++ app/public/index.js
@@ -1,5 +1,5 @@
 let username;
-let p = document.getElementById("p");
+const container = document.getElementById("container");
 
 fetch('/identity/isUserLoggedIn')
     .then((response) => response.json())
@@ -14,8 +14,35 @@ fetch('/identity/isUserLoggedIn')
     })
     .then((response) => response.json())
     .then((data) => {
-        console.log(data);
-        p.textContent = data.data;
+        //for(let post of data){
+        let post = data[0];
+            let username = post.username;
+            let caption = post.caption;
+            let timestamp = post.timestamp;
+            let outfit = post.outfit;
+            let comments = post.comments;
+            let likeCount = post.likeCount;
+            
+            for (let i = 1; i <= 5; i++) {
+                const cell = document.createElement('div');
+                cell.classList.add('cell');
+                cell.textContent = i;
+                container.appendChild(cell);
+            }
+
+            const likeAndDateBar = document.createElement('div');
+            likeAndDateBar.style.display = 'flex';
+            const likeHalf = document.createElement('div');
+            const dateHalf = document.createElement('div');
+            container.insertAdjacentElement('afterend', likeAndDateBar);
+            likeHalf.textContent = `Likes: ${likeCount}`;
+            likeAndDateBar.appendChild(likeHalf);
+            dateHalf.textContent = timestamp;
+            dateHalf.style.textAlign = right;
+            likeAndDateBar.appendChild(dateHalf);
+        //}
+
+        test.textContent = data[0];
     })
     .catch((error) => console.error('Error fetching request data:', error));
 
